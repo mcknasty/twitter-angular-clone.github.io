430c2f10c3a555582af92aad8b112db1f82cb88d
The Nasty <tmckeown@gmail.com>
2022-08-16 12:40:12 -0400

    Triming and moving core config files, removing dup confi files, and modifing the circle ci build process to test uncovered functionality

--- src/app/model/BasicRecord.ts
+++ src/app/model/BasicRecord.ts
@@ -57,10 +57,10 @@ class BasicRecord extends AbstractBasicRecord implements BasicRecordInterface {
 
   public static generateId(len: number = 0): string {
     const dec2hex = (dec: number) => {
-      return ('0' + dec.toString(16)).substr(-2);
+      return ('0' + dec.toString(16)).substring(-2);
     };
     const arr = new Uint8Array((len || 40) / 2);
-    window.crypto.getRandomValues(arr);
+    crypto.getRandomValues(arr);
     return Array.from(arr, dec2hex).join('');
   }
 }
