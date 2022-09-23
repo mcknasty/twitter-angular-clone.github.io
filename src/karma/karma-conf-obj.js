dfe705c69d69ff19d37ffa8376de56ac4c51fafb
The Nasty <tmckeown@gmail.com>
2022-09-23 19:10:14 -0400

    fixing codecov circleci orb

--- src/karma/karma-conf-obj.js
+++ src/karma/karma-conf-obj.js
@@ -31,7 +31,7 @@ module.exports.conf = {
     }
   },
   coverageReporter: {
-    dir: require('path').join(__dirname, '../coverage/twitter-clone'),
+    dir: require('path').join(__dirname, '../coverage'),
     subdir: '.',
     reporters: [
       { type: 'lcovonly' },
